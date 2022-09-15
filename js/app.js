dd7cd4442132743ad383ef6d91f62837173ebe98
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2022-09-15 13:50:45 -0300

    create function find secret word

---
+++ js/app.js
@@ -0,0 +1,68 @@
+// 
+const words = ["js", "java", "python"];
+let secretWord = "";
+
+const canvas = document.querySelector(".canvas");
+const board = canvas.getContext("2d");
+
+
+const btnStart = document.querySelector(".btn-start");
+const btnAdd = document.querySelector(".btn-add");
+// 
+const header = document.querySelector(".header");
+const btns = document.querySelector(".buttons");
+// section game
+const game  = document.querySelector(".game");
+const btnNew = document.querySelector(".btn-new");
+const btGiveUp = document.querySelector(".btn-giveUp");
+// section word
+const sectionWord = document.querySelector(".section-word");
+const btnSave = document.querySelector(".btn-save");
+// 
+
+// ----- functions -----------------------
+
+const findSecretWord = () => {
+  const word = words[ Math.floor( Math.random() * words.length ) ];
+  secretWord = word;
+  console.log( secretWord)
+}
+
+
+
+
+// ---- events buttons ------------------
+btnStart.addEventListener( "click", function() {
+ btns.style.display = "none";
+ game.style.display = "block";
+ game.classList.toggle("d-flex");
+ findSecretWord();
+});
+
+
+
+// show section word
+btnAdd.addEventListener( "click", function(){
+  sectionWord.style.display = "flex";
+  sectionWord.classList.toggle("d-flex")
+  btns.style.display = "none";
+
+});
+
+
+// 
+btnNew.addEventListener("click", function() {
+
+});
+
+btGiveUp.addEventListener("click", function(){
+  btns.style.display = "block";
+  game.style.display = "none";
+});
+
+// 
+btnSave.addEventListener("click", function(){
+  game.style.display = "flex";
+  sectionWord.style.display = "none";
+});
+
